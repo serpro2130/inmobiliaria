9f7ef06fb74b223e1ba4f8a530b7a5491876b65f
SERPROEMCAM <serproemcamseguridadprivada@gmail.com>
2022-01-14 08:14:50 -0500

    Start Web Inmobiliaria

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const imagenes = document.querySelectorAll(".propiedad__imagen");
+window.addEventListener("scroll", () => {
+  const scroll = this.scrollY / -20;
+  imagenes.forEach((imagen) => {
+    imagen.style.backgroundPositionY = `${scroll}px`;
+  });
+});
